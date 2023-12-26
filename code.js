5184153aa4b643862253b9ccd72c7d4b7b92bef6
R0YR00T <127723566+R0YR00T@users.noreply.github.com>
2023-12-26 13:14:20 +1100

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 let item = document.getElementById("no");
 
 function change() {
-    item.style.left = `${(Math.random() * window.innerWidth) - 500}px`;
-    item.style.top = `${(Math.random() * window.innerHeight) - 500}px`;
+    item.style.left = `${(Math.random() * window.innerWidth) - 300}px`;
+    item.style.top = `${(Math.random() * window.innerHeight) - 300}px`;
 }
 
 item.addEventListener("mouseover", change);
